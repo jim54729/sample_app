4ef71fc389f8c1107128911fb77befe33b42e56d
Jim Schwetz <jim54729@gmail.com>
2014-04-24 13:40:25 -0500

    forgot to save

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,19 @@ describe "User pages" do
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
+    
+    describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
 
     describe "with invalid information" do
       it "should not create a user" do
